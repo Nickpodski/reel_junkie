a757bd4c31fe450743d211146893f31a3a9a3686
William Cheausky <williamchenausky@yahoo.com>
2021-05-17 17:53:12 -0400

    get carousel to render

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar/MyNavBar';
-import TiffsTemporaryComponent from "./components/homePage/TiffsTemporaryComponent"
+import MoviesInCarousel from './components/MoviesInCarousel/MoviesInCarousel'
 // import { fetchMovies } from "../src/utils/API";
 
 
@@ -10,7 +10,7 @@ function App() {
     <div>
       <Navbar />
         {/* Added below code for testing */}
-        <TiffsTemporaryComponent />
+        <MoviesInCarousel />
     </div>
 
   );
---
+++ client/src/components/MoviesInCarousel/MoviesInCarousel.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "./MoviesInCarousel.css";
+import Carousel from "react-bootstrap/Carousel";
+import { moviesPlayingNow } from "../../utils/API";
+
+const MoviesInCarousel = () => {
+
+  const [nowPlaying, setNowPlaying] = useState([]);
+  useEffect(() => {
+    const getNowPlaying = async () => {
+      const items = await moviesPlayingNow();
+      setNowPlaying(items);
+    };
+    getNowPlaying();
+  }, []);
+const moviesInCarousel = nowPlaying.map((item, index) => {
+  return (
+    
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={item.poster}
+        alt={item.title}
+      />
+      <Carousel.Caption>
+        <h3>{item.title}</h3>
+        <p>{item.overview}</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  )
+})
+  return (
+    <div>
+      <Carousel fade>
+      {moviesInCarousel}
+      </Carousel>
+     </div>
+  );
+};
+
+export default MoviesInCarousel;
