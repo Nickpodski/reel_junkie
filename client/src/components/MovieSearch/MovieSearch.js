55ac3fc6b73861e7022d1376b823d93da6a35995
William Cheausky <williamchenausky@yahoo.com>
2021-05-20 11:09:05 -0400

    add all pages

--- client/src/components/MovieSearch/MovieSearch.js
+++ client/src/components/MovieSearch/MovieSearch.js
@@ -1,43 +1,41 @@
 import React from 'react'
-import "./MoviesInCarousel.css"
-import Card from "react-bootstrap/Card"
-import Nav from "react-bootstrap/Nav"
+import "./MovieSearch.css"
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 
 function MovieSearch() {
     return (
       <>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Nav variant="tabs" defaultActiveKey="#first">
-              <Nav.Item>
-                <Nav.Link href="#first">Plot</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#link">Add to Watch List</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#disabled">Reviews</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#link">Cast</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#link">Watched</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#link">Streaming</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Card.Body>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-        </Card>
-      </>
+    <Container>
+    <Image src="holder.js/100px250" fluid />
+    <Tabs defaultActiveKey="description" id="uncontrolled-tab-example">
+    <Tab eventKey="description" title="Descripton">
+
+            
+            </Tab>
+            <Tab eventKey="Reviews" title="Reviews">
+              
+            </Tab>
+            <Tab eventKey="Cast" title="Cast">
+            
+            </Tab>
+            <Tab eventKey="Add to Watch List" title="Contact">
+            
+            </Tab>
+            <Tab eventKey="Add to Watched List" title="Contact">
+            
+            </Tab>
+            <Tab eventKey="Streaming" title="Streaming">
+            
+            </Tab>
+          </Tabs>
+
+    </Container>
+    </>
     );
 }
 
-export default MovieSearch
+export default MovieSearch;
 
