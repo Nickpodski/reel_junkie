3f8d488688bdca02deb4cc6b08873acece986999
William Cheausky <williamchenausky@yahoo.com>
2021-05-21 11:35:57 -0400

    homepage styling

--- client/src/App.js
+++ client/src/App.js
@@ -32,6 +32,7 @@ function App() {
 
   return (
     <div>
+    
       <Router>
         <Navbar onChange={handleInputChange} onSubmit={handleSumbit} />
         <div>
--- client/src/components/NavBar/MyNavBar.js
+++ client/src/components/NavBar/MyNavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import "./MyNavBar.css";
-import {Nav, Button, Navbar, Form, FormControl} from "react-bootstrap";
+import "./MyNavBar.css";
+import {Nav, Button, Navbar, Form, FormControl, Media} from "react-bootstrap";
 import { Link, useHistory } from 'react-router-dom';
 
 
@@ -17,15 +17,22 @@ const NavBar = (props) => {
 
   return (
     <>
-      <Navbar bg="dark" variant="dark" className="justify-content-between">
-          <Navbar.Brand href="#home">Reel Junkies</Navbar.Brand>
+      <Navbar className="navbar" bg="black" variant="dark" sticky="top" className="navbar border-bottom border-warning justify-content-between">
+          <Navbar.Brand  as={Link} to="/home"><Media>
+<img
+  width={100}
+  height={75}
+  src="./images/reel-junkie-logo-2.jpg"
+  alt=""
+/>
+</Media></Navbar.Brand>
           <Form inline onSubmit={handleSumbit}>
-            <FormControl onChange={onChange} type="text" placeholder="Search" className="mr-sm-2 justify-content-center"/>
-            <Button variant="outline-info" onClick={handleSumbit}>Search</Button>
+            <FormControl onChange={onChange}  type="text" placeholder="Search" className="mr-sm-2 justify-content-center"/>
+            <Button variant="warning" onClick={handleSumbit}>Search</Button>
           </Form>
-        <Nav>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+        <Nav >
+          <Nav.Item >
+            <Nav.Link className = "link" as={Link} to="/login">Login</Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar>
