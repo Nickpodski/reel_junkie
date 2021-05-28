d5e555f2fb083e59f1d8d0352ad45dffeb8a3a26
Tianwes <willnotsuffice@gmail.com>
2021-05-28 15:22:54 -0400

    Credits scroll

--- client/src/components/Credits/Credits.js
+++ client/src/components/Credits/Credits.js
@@ -1,130 +1,221 @@
-import React from 'react'
-import "./Credits.css"
-import Container from "react-bootstrap/Container"
+import React from "react";
+import "./Credits.css";
+import Container from "react-bootstrap/Container";
 
 function Credits() {
-    return (
-        
-        <Container className="d-flex justidy-content-center">
-        <div>
+  return (
+    <Container className="wrapper d-flex justidy-content-center">
+      <ul>
+     <li> 
         Icons made by
-        <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons"
-          >Roundicons</a
+        <a
+          href="https://www.flaticon.com/authors/roundicons"
+          title="Roundicons"
         >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+          Roundicons
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/pause08" title="Pause08">Pause08</a>
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+        <a href="https://www.flaticon.com/authors/pause08" title="Pause08">
+          Pause08
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons"
-          >Smashicons</a
+        <a
+          href="https://www.flaticon.com/authors/smashicons"
+          title="Smashicons"
         >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+          Smashicons
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a>
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+        <a href="https://www.flaticon.com/authors/monkik" title="monkik">
+          monkik
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware"
-          >Good Ware</a
+        <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">
+          Good Ware
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
+        Icons made by
+        <a
+          href="https://www.flaticon.com/authors/smalllikeart"
+          title="smalllikeart"
         >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+          smalllikeart
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/smalllikeart" title="smalllikeart"
-          >smalllikeart</a
+        <a href="https://www.flaticon.com/authors/xnimrodx" title="xnimrodx">
+          xnimrodx
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
+        Icons made by
+        <a
+          href="https://www.flaticon.com/authors/icongeek26"
+          title="Icongeek26"
         >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+          Icongeek26
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/xnimrodx" title="xnimrodx"
-          >xnimrodx</a
+        <a href="https://www.flaticon.com/authors/catkuro" title="catkuro">
+          catkuro
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
+        Icons made by
+        <a href="https://www.freepik.com" title="Freepik">
+          Freepik
+        </a>{" "}
+        from
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
+        Icons made by
+        <a
+          href="https://www.flaticon.com/authors/darius-dan"
+          title="Darius Dan"
         >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+          Darius Dan
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26"
-          >Icongeek26</a
+        <a
+          href="https://www.flaticon.com/authors/roundicons"
+          title="Roundicons"
         >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+          Roundicons
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/catkuro" title="catkuro">catkuro</a>
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
-        Icons made by
-        <a href="https://www.freepik.com" title="Freepik">Freepik</a> from
-        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan"
-          >Darius Dan</a
+        <a
+          href="https://www.flaticon.com/authors/pongsakornred"
+          title="pongsakornRed"
         >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons"
-          >Roundicons</a
-        >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/pongsakornred" title="pongsakornRed"
-          >pongsakornRed</a
-        >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+          pongsakornRed
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
         <a
           href="https://www.flaticon.com/authors/nikita-golubev"
           title="Nikita Golubev"
-          >Nikita Golubev</a
         >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+          Nikita Golubev
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a>
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+        <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">
+          Eucalyp
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26"
-          >Icongeek26</a
+        <a
+          href="https://www.flaticon.com/authors/icongeek26"
+          title="Icongeek26"
         >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+          Icongeek26
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.flaticon.com/authors/iconixar" title="iconixar"
-          >iconixar</a
-        >
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      <div>
+        <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">
+          iconixar
+        </a>
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     <li>
         Icons made by
-        <a href="https://www.freepik.com" title="Freepik">Freepik</a> from
-        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
-      </Container>
-      
-    )
+        <a href="https://www.freepik.com" title="Freepik">
+          Freepik
+        </a>{" "}
+        from
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </li>
+     </ul>
+    </Container>
+  );
 }
 
-export default Credits
+export default Credits;
