2620a3bc392345754f64b03c71bec70e7ecf8495
Tianwes <willnotsuffice@gmail.com>
2021-05-28 21:07:47 -0400

    Credits page working, needs a few name updates

--- client/src/components/Credits/Credits.js
+++ client/src/components/Credits/Credits.js
@@ -1,220 +1,237 @@
 import React from "react";
 import "./Credits.css";
-import Container from "react-bootstrap/Container";
 
 function Credits() {
   return (
-    <Container className="wrapper d-flex justidy-content-center">
+    <div className="wrapper">
       <ul>
-     <li> 
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/roundicons"
-          title="Roundicons"
-        >
-          Roundicons
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/pause08" title="Pause08">
-          Pause08
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/smashicons"
-          title="Smashicons"
-        >
-          Smashicons
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/monkik" title="monkik">
-          monkik
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">
-          Good Ware
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/smalllikeart"
-          title="smalllikeart"
-        >
-          smalllikeart
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/xnimrodx" title="xnimrodx">
-          xnimrodx
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/icongeek26"
-          title="Icongeek26"
-        >
-          Icongeek26
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/catkuro" title="catkuro">
-          catkuro
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a href="https://www.freepik.com" title="Freepik">
-          Freepik
-        </a>{" "}
-        from
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/darius-dan"
-          title="Darius Dan"
-        >
-          Darius Dan
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/roundicons"
-          title="Roundicons"
-        >
-          Roundicons
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/pongsakornred"
-          title="pongsakornRed"
-        >
-          pongsakornRed
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/nikita-golubev"
-          title="Nikita Golubev"
-        >
-          Nikita Golubev
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">
-          Eucalyp
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/icongeek26"
-          title="Icongeek26"
-        >
-          Icongeek26
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">
-          iconixar
-        </a>
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     <li>
-        Icons made by
-        <a href="https://www.freepik.com" title="Freepik">
-          Freepik
-        </a>{" "}
-        from
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </li>
-     </ul>
-    </Container>
+        <li className="title">Reel Junkies</li>
+        <li className="broughtToYou">Brought to by</li>
+        <li>
+        <a 
+        className="devCreditsLinks"
+        href="https://github.com/Nickpodski" 
+        target="_blank" rel="noreferrer">Nick Podniestrzanski</a>
+        </li>
+        <li>
+        <a 
+        className="devCreditsLinks"
+        href="https://github.com/wchenausky"
+        target="_blank" rel="noreferrer">William Chenausky</a>
+        </li>
+        <li>
+        <a 
+        className="devCreditsLinks"
+        href="https://github.com/Tianwes"
+        target="_blank" rel="noreferrer">Tiffany Weaver</a>
+        </li>
+        <br />
+        <br />
+        <h3>Reel Junkies Logo Designed by .......</h3>
+        <br />
+        <li>
+          <span className="madeBy">Icons from </span>
+        </li>
+        <li>
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/"
+            title="Flaticon"
+            target="_blank" rel="noreferrer"
+          >
+            www.flaticon.com
+          </a>
+        </li>
+        <li>
+          <span className="madeBy">were made by</span>
+        </li>
+        <br />
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/roundicons"
+            title="Roundicons"
+            target="_blank" rel="noreferrer"
+          >
+            Roundicons
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/pause08"
+            title="Pause08"
+            target="_blank" rel="noreferrer"
+          >
+            Pause08
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/smashicons"
+            title="Smashicons"
+            target="_blank" rel="noreferrer"
+          >
+            Smashicons
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/monkik"
+            title="monkik"
+            target="_blank" rel="noreferrer"
+          >
+            monkik
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/good-ware"
+            title="Good Ware"
+            target="_blank" rel="noreferrer"
+          >
+            Good Ware
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/smalllikeart"
+            title="smalllikeart"
+            target="_blank" rel="noreferrer"
+          >
+            smalllikeart
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/xnimrodx"
+            title="xnimrodx"
+            target="_blank" rel="noreferrer"
+          >
+            xnimrodx
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/icongeek26"
+            title="Icongeek26"
+            target="_blank" rel="noreferrer"
+          >
+            Icongeek26
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/catkuro"
+            title="catkuro"
+            target="_blank" rel="noreferrer"
+          >
+            catkuro
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.freepik.com"
+            title="Freepik"
+            target="_blank" rel="noreferrer"
+          >
+            Freepik
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/darius-dan"
+            title="Darius Dan"
+            target="_blank" rel="noreferrer"
+          >
+            Darius Dan
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/roundicons"
+            title="Roundicons"
+            target="_blank" rel="noreferrer"
+          >
+            Roundicons
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/pongsakornred"
+            title="pongsakornRed"
+            target="_blank" rel="noreferrer"
+          >
+            pongsakornRed
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/nikita-golubev"
+            title="Nikita Golubev"
+            target="_blank" rel="noreferrer"
+          >
+            Nikita Golubev
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/eucalyp"
+            title="Eucalyp"
+            target="_blank" rel="noreferrer"
+          >
+            Eucalyp
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/icongeek26"
+            title="Icongeek26"
+            target="_blank" rel="noreferrer"
+          >
+            Icongeek26
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.flaticon.com/authors/iconixar"
+            title="iconixar"
+            target="_blank" rel="noreferrer"
+          >
+            iconixar
+          </a>
+        </li>
+        <li className="artistName">
+          <a
+            className="creditsLinks"
+            href="https://www.freepik.com"
+            title="Freepik"
+            target="_blank" rel="noreferrer"
+          >
+            Freepik
+          </a>
+        </li>
+        <br />
+        <br />
+        <br />
+        <li>Thanks for watching!</li>
+      </ul>
+    </div>
   );
 }
 
