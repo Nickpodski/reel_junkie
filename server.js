9e3907c6ac274ec8167547e3ff293072c616445c
Nickpodski <Nickpodski@gmail.com>
2021-06-02 13:55:36 -0400

    testing this

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reelJunkiesDB",
