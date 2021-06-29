a5e2bcb92d154a5044fd039681f4a85c5de5a0ef
Nickpodski <Nickpodski@gmail.com>
2021-06-29 17:36:57 -0400

    recreating old user

--- client/src/utils/updateUser.js
+++ client/src/utils/updateUser.js
@@ -7,32 +7,35 @@ export const updateUserRuntime = async (data) => {
   const userData = data;
   const userMW = data.movies_watched;
   if (userMW.length > 0) {
-    let res = 'movie_runtime' in userMW;
-    if (!res) {
-      userMW.map(async (item, index) => {
-        let movieRunTime = item.movie_runtime;
-        if (!movieRunTime) {
-          const newRunTime = await getRunTime(item.movie_id);
-          console.log(newRunTime);
-          userMW[index].movie_runtime = newRunTime;
-        }
-      });
-      axios.put('/api/user/addmoviewatched', { email, userMW })
-        .then(() => {
-          console.log('Updated!');
-        })
-        .catch((error) => {
-          if (error.response) {
-            console.log(error.response.data.message);
-          } else if (error.request) {
-            console.log('Server connection Issue!');
-          } else {
-            console.log(error.message);
-          }
-        })
-      return userData;
-    } else {
-      return null;
-    }
+     let checkRuntime = userMW.filter(e => e['movie_runtime']);
+     console.log(checkRuntime);
+    // if (checkRuntime > 0) {
+    //   return null;
+    // } else {
+    //   userMW.map(async (item, index) => {
+    //     let movieRunTime = item.movie_runtime;
+    //     if (!movieRunTime) {
+    //       const newRunTime = await getRunTime(item.movie_id);
+    //       console.log(newRunTime);
+    //       userMW[index].movie_runtime = newRunTime;
+    //     }
+    //   });
+    //   axios.put('/api/user/addmoviewatched', { email, userMW })
+    //     .then(() => {
+    //       console.log('Updated!');
+    //     })
+    //     .catch((error) => {
+    //       if (error.response) {
+    //         console.log(error.response.data.message);
+    //       } else if (error.request) {
+    //         console.log('Server connection Issue!');
+    //       } else {
+    //         console.log(error.message);
+    //       }
+    //     });
+    //   return userData;
+    // }
+  } else {
+    return null;
   }
 }
